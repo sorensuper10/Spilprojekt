a16cb4913dd0fabb608129cb5cd96b7fe0bdf4e5
sorensuper10 <soren_super@hotmail.com>
2024-01-16 12:44:17 +0100

    Initial commit

---
+++ src/main/java/components/Flagpole.java
@@ -0,0 +1,22 @@
+package components;
+
+import jade.GameObject;
+import org.jbox2d.dynamics.contacts.Contact;
+import org.joml.Vector2f;
+import util.AssetPool;
+
+public class Flagpole extends Component {
+    private boolean isTop = false;
+
+    public Flagpole(boolean isTop) {
+        this.isTop = isTop;
+    }
+
+    @Override
+    public void beginCollision(GameObject obj, Contact contact, Vector2f contactNormal) {
+        PlayerController playerController = obj.getComponent(PlayerController.class);
+        if (playerController != null) {
+            playerController.playWinAnimation(this.gameObject);
+        }
+    }
+}
